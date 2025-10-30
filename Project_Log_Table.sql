7a27f46b7df9202d9bc579d0a6941bdc334b9829
KevinHonakerDBA <kevincatfish82@gmail.com>
2025-10-30 14:43:40 -0400

    Update Project_Log_Table.sql

--- Project_Log_Table.sql
+++ Project_Log_Table.sql
@@ -9,5 +9,11 @@ CREATE TABLE project_log (
   CONSTRAINT PK_Project PRIMARY KEY (ProjectID)
 );
 
-      
+ -- DML to insert initial data into the Project_Log table
+INSERT INTO Project_Log (ProjectID, ProjectName, GitHubURL, StartDate, Status)
+VALUES 
+(1, 'Personal Portfolio Website', 'https://github.com/KevinHonakerDBA/.github.io', '2025-10-30', 'In Progress');
+
+-- Select the data to confirm the insert
+SELECT * FROM Project_Log;     
       
